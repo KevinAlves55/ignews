6ac96bea87a856689188e7e75d294002fecd8b28
KevinAlves55 <programador.kevin4773@gmail.com>
2022-10-27 22:21:11 -0300

    fix: Ajustando o NextAuth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
+import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github"
 
-export const authOptions = {
-  // Configure one or more authentication providers
+export default NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
@@ -13,4 +13,4 @@ export const authOptions = {
       },
     }),
   ]
-}
+})
