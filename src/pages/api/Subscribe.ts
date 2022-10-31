607a28606a0ecc422b1147498bf50101b982bb17
KevinAlves55 <programador.kevin4773@gmail.com>
2022-10-31 17:16:16 -0300

    fix: Corrigindo o nome do arquivo

--- src/pages/api/Subscribe.ts
+++ src/pages/api/Subscribe.ts
@@ -9,7 +9,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     const stripeCustomer = await stripe.customers.create({
       email: session.user.email,
-      // metadata
     })
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
